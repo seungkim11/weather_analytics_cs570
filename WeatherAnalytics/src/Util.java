2de3213bd32066671930006188e5e1ca18829184
Seung Kim <seungkim11@gmail.com>
2016-03-01 23:48:51 -0800

    Delete Util.java

--- WeatherAnalytics/src/Util.java
+++
@@ -1,6 +0,0 @@
-
-public class Util {
-  public static final String datafile1 = "D:/dev/workspace/nodeJS/cs570/data1.json";
-  public static final String datafile2 = "D:/dev/workspace/nodeJS/cs570/data2.json";
-  
-}
